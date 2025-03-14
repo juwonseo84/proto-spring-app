9f26f919aee71f9b0daa2b82c1f8f12fc215674e
juwonseo84 <newschool84@naver.com>
2025-03-14 17:26:29 +0900

    first-commit

---
+++ src/main/java/com/demo/protospringapp/ProtoSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.protospringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtoSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/protospringapp/ProtoSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.protospringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
